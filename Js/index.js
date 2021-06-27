e3483823a51ed90daca2faa5ce5e5358c970b5eb
ricardo <ricky.zelada.a@gmail.com>
2021-06-27 01:54:10 -0400

    Creacion de modulo habitaciones
    Creacion de tabs con html, css y js, archivo nuevo index.js

---
+++ Js/index.js
@@ -0,0 +1,43 @@
+
+let content_1 = document.getElementById("content_one");
+let content_2 = document.getElementById("content_two");
+let content_3 = document.getElementById("content_three");
+let btn_1 = document.getElementById("btn_one");
+let btn_2 = document.getElementById("btn_two");
+let btn_3 = document.getElementById("btn_three");
+
+function singleRoom(){
+        content_1.style.transform = "translateX(0)";
+        content_2.style.transform = "translateX(100%)";
+        content_3.style.transform = "translateX(100%)";
+        btn_1.style.color = "#FC9A32";
+        btn_2.style.color = "#000000";
+        btn_3.style.color = "#000000";
+        content_1.style.transitionDelay = "0.3s";
+        content_2.style.transitionDelay = "0s";
+        content_3.style.transitionDelay = "0s";
+    }
+
+function dobleRoom(){
+        content_1.style.transform = "translateX(100%)";
+        content_2.style.transform = "translateX(0)";
+        content_3.style.transform = "translateX(100%)";
+        btn_2.style.color = "#FC9A32";
+        btn_1.style.color = "#000000";
+        btn_3.style.color = "#000000";
+        content_1.style.transitionDelay = "0s";
+        content_2.style.transitionDelay = "0.3s";
+        content_3.style.transitionDelay = "0s";
+    }
+
+function familyRoom(){
+        content_1.style.transform = "translateX(100%)";
+        content_2.style.transform = "translateX(100%)";
+        content_3.style.transform = "translateX(0)";
+        btn_3.style.color = "#FC9A32";
+        btn_2.style.color = "#000000";
+        btn_1.style.color = "#000000";
+        content_1.style.transitionDelay = "0s";
+        content_2.style.transitionDelay = "0s";
+        content_3.style.transitionDelay = "0.3s";
+    }
